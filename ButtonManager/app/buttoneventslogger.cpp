74bfe32880f687a3bd95ac5dd81da4ee0f56dfbe
selligZeittom <gilmottiez@gmail.com>
2019-03-27 14:46:38 +0100

    state machine logger

--- ButtonManager/app/buttoneventslogger.cpp
+++ ButtonManager/app/buttoneventslogger.cpp
@@ -7,9 +7,11 @@
 
 #include <app/buttoneventslogger.h>
 
+#define debugMode 1
+
 ButtonEventsLogger::ButtonEventsLogger() {
 	// TODO Auto-generated constructor stub
-
+	currentState = STATE_INITIAL;
 }
 
 ButtonEventsLogger::~ButtonEventsLogger() {
@@ -17,7 +19,85 @@ ButtonEventsLogger::~ButtonEventsLogger() {
 }
 
 void ButtonEventsLogger::onButtonShortPressed(ButtonIndex buttonIndex) {
+
 }
 
 void ButtonEventsLogger::onButtonLongPressed(ButtonIndex buttonIndex) {
+
+}
+
+XFEventStatus ButtonEventsLogger::processEvent() {
+	XFEventStatus eventStatus = XFEventStatus::Unknown;
+
+	switch (currentState) {
+	case STATE_INITIAL:
+		if (getCurrentEvent()->getEventType() == XFEvent::Initial) {
+			currentState = STATE_WAIT;
+			Trace::out("[ButtonEventsLogger] : on the go to state wait");
+			eventStatus = XFEventStatus::Consumed;
+		}
+
+		break;
+	case STATE_WAIT:
+		if (getCurrentEvent()->getEventType() == XFEvent::Event
+				&& getCurrentEvent()->getId() == EVENT_ID_SHORT) {
+			currentState = STATE_SHORT_PRESS;
+			Trace::out("[ButtonEventsLogger] : on the go to state short press");
+			eventStatus = XFEventStatus::Consumed;
+		} else if (getCurrentEvent()->getEventType() == XFEvent::Event
+				&& getCurrentEvent()->getId() == EVENT_ID_LONG) {
+			currentState = STATE_LONG_PRESS;
+			Trace::out("[ButtonEventsLogger] : on the go to state long press");
+			eventStatus = XFEventStatus::Consumed;
+		}
+#if (debugMode == 1)
+		if (getCurrentEvent()->getEventType() == XFEvent::Timeout && getCurrentEvent()->getId() == EVENT_DB_SHORT) {
+			currentState = STATE_SHORT_PRESS;
+			Trace::out("[ButtonEventsLogger] : on the go to state short press");
+			eventStatus = XFEventStatus::Consumed;
+		}
+		if (getCurrentEvent()->getEventType() == XFEvent::Timeout && getCurrentEvent()->getId() == EVENT_DB_LONG) {
+			currentState = STATE_LONG_PRESS;
+			Trace::out("[ButtonEventsLogger] : on the go to state long press");
+			eventStatus = XFEventStatus::Consumed;
+		}
+#endif
+		break;
+	case STATE_SHORT_PRESS:
+		if (getCurrentEvent()->getEventType() == XFEvent::Event
+				&& getCurrentEvent()->getId() == EVENT_ID_WAIT) {
+			currentState = STATE_WAIT;
+			Trace::out("[ButtonEventsLogger] : on the go to state wait");
+			eventStatus = XFEventStatus::Consumed;
+		}
+#if (debugMode == 1)
+		if (getCurrentEvent()->getEventType() == XFEvent::Timeout
+				&& getCurrentEvent()->getId() == EVENT_DB_WAIT) {
+			currentState = STATE_WAIT;
+			Trace::out("[ButtonEventsLogger] : on the go to state wait");
+			eventStatus = XFEventStatus::Consumed;
+		}
+#endif
+		break;
+	case STATE_LONG_PRESS:
+		if (getCurrentEvent()->getEventType() == XFEvent::Event
+				&& getCurrentEvent()->getId() == EVENT_ID_WAIT) {
+			currentState = STATE_WAIT;
+			Trace::out("[ButtonEventsLogger] : on the go to state wait");
+			eventStatus = XFEventStatus::Consumed;
+		}
+#if (debugMode == 1)
+		if (getCurrentEvent()->getEventType() == XFEvent::Timeout
+				&& getCurrentEvent()->getId() == EVENT_DB_WAIT) {
+			currentState = STATE_WAIT;
+			Trace::out("[ButtonEventsLogger] : on the go to state wait");
+			eventStatus = XFEventStatus::Consumed;
+		}
+#endif
+		break;
+	default:
+		break;
+	}
+
+	return eventStatus;
 }
--- ButtonManager/app/buttoneventslogger.h
+++ ButtonManager/app/buttoneventslogger.h
@@ -9,8 +9,11 @@
 #define BUTTONEVENTSLOGGER_H_
 
 #include <interface/buttoneventshandlerobserver.h>
+#include "mdw/trace/trace.h"
+#include "xf/behavior.h"
 
-class ButtonEventsLogger: public interface::ButtonEventsHandlerObserver {
+class ButtonEventsLogger: public interface::ButtonEventsHandlerObserver, XFBehavior
+{
 public:
 	ButtonEventsLogger();
 	virtual ~ButtonEventsLogger();
@@ -19,6 +22,31 @@ public:
 	virtual void onButtonShortPressed(ButtonIndex buttonIndex); ///< @brief Called by the subject upon a button short pressed.
 	virtual void onButtonLongPressed(ButtonIndex buttonIndex); ///< @brief Called by the subject upon a button long pressed.
 
+	//from the state machine class
+	virtual XFEventStatus processEvent();
+
+protected:
+
+	typedef enum {
+		STATE_INITIAL = 0,
+		STATE_WAIT = 1,
+		STATE_SHORT_PRESS = 2,
+		STATE_LONG_PRESS = 3
+	} STATE_LOGGER;
+
+	enum {
+		EVENT_ID_SHORT = 0,
+		EVENT_ID_LONG = 1,
+		EVENT_ID_WAIT = 2
+	};
+
+	enum {
+		EVENT_DB_SHORT = 0,
+		EVENT_DB_LONG = 1,
+		EVENT_DB_WAIT = 2
+	};
+
+	STATE_LOGGER currentState;
 };
 
 #endif /* BUTTONEVENTSLOGGER_H_ */
